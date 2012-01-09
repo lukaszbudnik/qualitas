db96ac965a278e14cefca2e2f664235d137d6cc1
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-01-09 09:39:34 +0100

    fixed all checkstyle errors

--- qualitas-engines-ode-deployment/src/main/java/com/google/code/qualitas/engines/ode/deployment/OdeProcessBundleDeployer.java
+++ qualitas-engines-ode-deployment/src/main/java/com/google/code/qualitas/engines/ode/deployment/OdeProcessBundleDeployer.java
@@ -8,65 +8,83 @@ import com.google.code.qualitas.engines.api.deployment.ProcessBundleDeploymentRe
 /**
  * The Class OdeProcessBundleDeployer.
  */
-public class OdeProcessBundleDeployer implements ProcessBundleDeployer<OdeProcessBundle> {
+public class OdeProcessBundleDeployer implements
+        ProcessBundleDeployer<OdeProcessBundle> {
 
-	/** The default deployment service endpoint. */
-	private String defaultDeploymentServiceEndpoint;
-	
-	/** The deployment service endpoint. */
-	private String deploymentServiceEndpoint;
+    /** The default deployment service endpoint. */
+    private String defaultDeploymentServiceEndpoint;
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer#deploy(com.google.code.qualitas.engines.api.core.ProcessBundle)
-	 */
-	@Override
-	public ProcessBundleDeploymentResult deploy(OdeProcessBundle processBundle) {
-		
-		String odeUrl;
-		if (this.deploymentServiceEndpoint == null) {
-			odeUrl = this.deploymentServiceEndpoint;
-		} else {
-			odeUrl = this.defaultDeploymentServiceEndpoint;
-		}
-		
-		OdeDeploymentManager manager = new OdeDeploymentManager(odeUrl);
-		
-		ProcessBundleDeploymentResult bundleDeploymentResult = new ProcessBundleDeploymentResult();
+    /** The deployment service endpoint. */
+    private String deploymentServiceEndpoint;
 
-		try {
-			manager.deploy(processBundle);
-		} catch (RemoteDeploymentException e) {
-			bundleDeploymentResult.setSuccess(false);
-			bundleDeploymentResult.setErrorMessage(e.getMessage());
-		}
-		
-		bundleDeploymentResult.setSuccess(true);
-		
-		return bundleDeploymentResult;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer
+     * #deploy(com.google.code.qualitas.engines.api.core.ProcessBundle)
+     */
+    @Override
+    public ProcessBundleDeploymentResult deploy(OdeProcessBundle processBundle) {
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer#isSupported(com.google.code.qualitas.engines.api.core.ProcessBundle)
-	 */
-	@Override
-	public boolean isSupported(ProcessBundle processBundle) {
-		return (processBundle instanceof OdeProcessBundle);
-	}
+        String odeUrl;
+        if (this.deploymentServiceEndpoint == null) {
+            odeUrl = this.deploymentServiceEndpoint;
+        } else {
+            odeUrl = this.defaultDeploymentServiceEndpoint;
+        }
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer#setDefaultDeploymentServiceEndpoint(java.lang.String)
-	 */
-	@Override
-	public void setDefaultDeploymentServiceEndpoint(String defaultDeploymentServiceEndpoint) {
-		this.defaultDeploymentServiceEndpoint = defaultDeploymentServiceEndpoint;
-	}
+        OdeDeploymentManager manager = new OdeDeploymentManager(odeUrl);
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer#setDeploymentServiceEndpoint(java.lang.String)
-	 */
-	@Override
-	public void setDeploymentServiceEndpoint(String deploymentServiceEndpoint) {
-		this.deploymentServiceEndpoint = deploymentServiceEndpoint;
-	}
-	
+        ProcessBundleDeploymentResult bundleDeploymentResult = new ProcessBundleDeploymentResult();
+
+        try {
+            manager.deploy(processBundle);
+        } catch (RemoteDeploymentException e) {
+            bundleDeploymentResult.setSuccess(false);
+            bundleDeploymentResult.setErrorMessage(e.getMessage());
+        }
+
+        bundleDeploymentResult.setSuccess(true);
+
+        return bundleDeploymentResult;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer
+     * #isSupported(com.google.code.qualitas.engines.api.core.ProcessBundle)
+     */
+    @Override
+    public boolean isSupported(ProcessBundle processBundle) {
+        return (processBundle instanceof OdeProcessBundle);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer
+     * #setDefaultDeploymentServiceEndpoint(java.lang.String)
+     */
+    @Override
+    public void setDefaultDeploymentServiceEndpoint(
+            String defaultDeploymentServiceEndpoint) {
+        this.defaultDeploymentServiceEndpoint = defaultDeploymentServiceEndpoint;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.deployment.ProcessBundleDeployer
+     * #setDeploymentServiceEndpoint(java.lang.String)
+     */
+    @Override
+    public void setDeploymentServiceEndpoint(String deploymentServiceEndpoint) {
+        this.deploymentServiceEndpoint = deploymentServiceEndpoint;
+    }
+
 }
