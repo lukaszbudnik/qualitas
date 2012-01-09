941e0d124043c112322a78fa882ba8456ab42f4f
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-01-09 09:38:39 +0100

    fixed all checkstyle error

--- qualitas-engines-api-core/src/main/java/com/google/code/qualitas/engines/api/core/AbstractProcessBundle.java
+++ qualitas-engines-api-core/src/main/java/com/google/code/qualitas/engines/api/core/AbstractProcessBundle.java
@@ -5,39 +5,55 @@ package com.google.code.qualitas.engines.api.core;
  */
 public abstract class AbstractProcessBundle implements ProcessBundle {
 
-	/** The main process name. */
-	protected String mainProcessName;
+    /** The main process name. */
+    private String mainProcessName;
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.core.ProcessBundle#setMainProcessName(java.lang.String)
-	 */
-	@Override
-	public void setMainProcessName(String mainProcessName) {
-		this.mainProcessName = mainProcessName;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.core.ProcessBundle#setMainProcessName
+     * (java.lang.String)
+     */
+    @Override
+    public void setMainProcessName(String mainProcessName) {
+        this.mainProcessName = mainProcessName;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.core.ProcessBundle#getMainProcessName()
-	 */
-	@Override
-	public String getMainProcessName() {
-		return mainProcessName;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.core.ProcessBundle#getMainProcessName
+     * ()
+     */
+    @Override
+    public String getMainProcessName() {
+        return mainProcessName;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.core.ProcessBundle#getQualitasDescriptor()
-	 */
-	@Override
-	public Entry getQualitasDescriptor() {
-		return getEntry(QUALITAS_DESCRIPTOR_NAME);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.core.ProcessBundle#getQualitasDescriptor
+     * ()
+     */
+    @Override
+    public Entry getQualitasDescriptor() {
+        return getEntry(QUALITAS_DESCRIPTOR_NAME);
+    }
 
-	/* (non-Javadoc)
-	 * @see com.google.code.qualitas.engines.api.core.ProcessBundle#isInstrumentable()
-	 */
-	@Override
-	public boolean isInstrumentable() {
-		return getQualitasDescriptor() != null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.code.qualitas.engines.api.core.ProcessBundle#isInstrumentable
+     * ()
+     */
+    @Override
+    public boolean isInstrumentable() {
+        return getQualitasDescriptor() != null;
+    }
 
 }
--- qualitas-engines-api-core/src/main/java/com/google/code/qualitas/engines/api/core/ProcessBundle.java
+++ qualitas-engines-api-core/src/main/java/com/google/code/qualitas/engines/api/core/ProcessBundle.java
@@ -5,94 +5,94 @@ package com.google.code.qualitas.engines.api.core;
  */
 public interface ProcessBundle {
 
-	/**
-	 * Every uploaded archive which user want to enhance should have qualitas
-	 * descriptor.
-	 */
-	String QUALITAS_DESCRIPTOR_NAME = "qualitas.xml";
+    /**
+     * Every uploaded archive which user want to enhance should have qualitas
+     * descriptor.
+     */
+    String QUALITAS_DESCRIPTOR_NAME = "qualitas.xml";
 
-	/**
-	 * Sets the bundle.
-	 * 
-	 * @param bundle
-	 *            the contents of bundle
-	 */
-	void setBundle(byte[] bundle);
+    /**
+     * Sets the bundle.
+     * 
+     * @param bundle
+     *            the contents of bundle
+     */
+    void setBundle(byte[] bundle);
 
-	/**
-	 * Builds the bundle.
-	 * 
-	 * @return the byte[]
-	 */
-	byte[] buildBundle();
+    /**
+     * Builds the bundle.
+     * 
+     * @return the byte[]
+     */
+    byte[] buildBundle();
 
-	/**
-	 * Sets the main process name.
-	 * 
-	 * @param mainProcessName
-	 *            the new main process name
-	 */
-	void setMainProcessName(String mainProcessName);
+    /**
+     * Sets the main process name.
+     * 
+     * @param mainProcessName
+     *            the new main process name
+     */
+    void setMainProcessName(String mainProcessName);
 
-	/**
-	 * Gets the main process name.
-	 * 
-	 * @return the main process name
-	 */
-	String getMainProcessName();
+    /**
+     * Gets the main process name.
+     * 
+     * @return the main process name
+     */
+    String getMainProcessName();
 
-	/**
-	 * Gets the qualitas descriptor.
-	 * 
-	 * @return the qualitas descriptor
-	 */
-	Entry getQualitasDescriptor();
+    /**
+     * Gets the qualitas descriptor.
+     * 
+     * @return the qualitas descriptor
+     */
+    Entry getQualitasDescriptor();
 
-	/**
-	 * Checks if is instrumentable. Returns true if scenario bundle contains
-	 * qualitas descriptor.
-	 * 
-	 * @return true, if is instrumentable
-	 */
-	boolean isInstrumentable();
+    /**
+     * Checks if is instrumentable. Returns true if scenario bundle contains
+     * qualitas descriptor.
+     * 
+     * @return true, if is instrumentable
+     */
+    boolean isInstrumentable();
 
-	/**
-	 * Gets entry by name.
-	 * 
-	 * @param name
-	 *            the name
-	 * @return the entry
-	 */
-	Entry getEntry(String name);
+    /**
+     * Gets entry by name.
+     * 
+     * @param name
+     *            the name
+     * @return the entry
+     */
+    Entry getEntry(String name);
 
-	/**
-	 * Adds given entry.
-	 * 
-	 * @param entry
-	 *            the entry
-	 */
-	void addEntry(Entry entry);
+    /**
+     * Adds given entry.
+     * 
+     * @param entry
+     *            the entry
+     */
+    void addEntry(Entry entry);
 
-	/**
-	 * Removes given entry.
-	 * 
-	 * @param entryName
-	 *            the entry name
-	 */
-	void removeEntry(String entryName);
+    /**
+     * Removes given entry.
+     * 
+     * @param entryName
+     *            the entry name
+     */
+    void removeEntry(String entryName);
 
-	/**
-	 * Renames existing entry.
-	 * 
-	 * @param oldName
-	 *            the old name
-	 * @param newName
-	 *            the new name
-	 */
-	void renameEntry(String oldName, String newName);
+    /**
+     * Renames existing entry.
+     * 
+     * @param oldName
+     *            the old name
+     * @param newName
+     *            the new name
+     */
+    void renameEntry(String oldName, String newName);
 
-	/**
-	 * Releases and removes all allocated resources..
-	 */
-	void cleanUp();
+    /**
+     * Releases and removes all allocated resources..
+     */
+    void cleanUp();
 }
