d165e9b69ae67e98d044ff71d80099e21fb90478
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-01-10 16:34:09 +0100

    added integration tests

---
+++ trunk/qualitas-engines-ode-deployment/src/test/java/com/google/code/qualitas/engines/api/deployment/OdeProcessBundleUndeployerITCase.java
@@ -0,0 +1,51 @@
+package com.google.code.qualitas.engines.api.deployment;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.code.qualitas.engines.api.core.OdeProcessBundle;
+import com.google.code.qualitas.engines.ode.deployment.OdeProcessBundleUndeployer;
+
+public class OdeProcessBundleUndeployerITCase {
+
+	private static OdeProcessBundle odeProcessBundle;
+	private static OdeProcessBundleUndeployer odeUndeployer;
+
+	@BeforeClass
+	public static void setUpClass() throws IOException {
+		odeProcessBundle = new OdeProcessBundle();
+		odeProcessBundle.setMainProcessName("XhHelloWorld2");
+		// undeployer
+		odeUndeployer = new OdeProcessBundleUndeployer();
+		odeUndeployer
+				.setDeploymentServiceEndpoint("http://localhost:9090/ode/processes/DeploymentService");
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		odeProcessBundle.cleanUp();
+	}
+
+	@Test
+	public void testSetDeploymentServiceEndpoint() {
+		odeUndeployer
+				.setDeploymentServiceEndpoint("http://localhost:9090/ode/processes/DeploymentService");
+	}
+
+	@Test
+	public void testSetDefaultDeploymentServiceEndpoint() {
+		odeUndeployer
+				.setDefaultDeploymentServiceEndpoint("http://localhost:9090/ode/processes/DeploymentService");
+	}
+
+	@Test
+	public void testUndeploy() {
+		ProcessBundleDeploymentResult result = odeUndeployer.undeploy(odeProcessBundle);
+		Assert.assertTrue(result.isSuccess());
+	}
+
+}
