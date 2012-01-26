e72f3fbfbc8cb31132816f2fbf9b7a3c4abaee8f
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-01-26 21:04:49 +0100

    all changes related to refactoring in qualitas-engines-api-core

--- qualitas-engines-ode-invocation/src/main/java/com/google/code/qualitas/engines/ode/invocation/OdeInvoker.java
+++ qualitas-engines-ode-invocation/src/main/java/com/google/code/qualitas/engines/ode/invocation/OdeInvoker.java
@@ -16,7 +16,8 @@ public class OdeInvoker extends AbstractWebServiceInvoker {
      * .google.code.qualitas.engines.api.core.ProcessType)
      */
     public boolean isSupported(ProcessType processType) {
-        return processType == ProcessType.WS_BPEL_1_0 || processType == ProcessType.WS_BPEL_2_0;
+        return processType == ProcessType.WS_BPEL_1_0_APACHE_ODE
+                || processType == ProcessType.WS_BPEL_2_0_APACHE_ODE;
     }
 
 }
