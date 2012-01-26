1ccad12ab13d3fddfb79a9bf057eadb793f7ad82
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-01-26 20:58:17 +0100

    all changes related to refactoring in qualitas-engines-api-core

--- qualitas-engines-api-instrumentation/src/main/java/com/google/code/qualitas/engines/api/instrumentation/Instrumentor.java
+++ qualitas-engines-api-instrumentation/src/main/java/com/google/code/qualitas/engines/api/instrumentation/Instrumentor.java
@@ -1,28 +1,20 @@
 package com.google.code.qualitas.engines.api.instrumentation;
 
-import com.google.code.qualitas.engines.api.core.ProcessBundle;
+import com.google.code.qualitas.engines.api.component.Component;
+import com.google.code.qualitas.engines.api.core.Bundle;
 
 /**
  * The Interface ProcessBundleInstrumentor.
  *
- * @param <T> the generic type
  */
-public interface Instrumentor<T extends ProcessBundle> {
+public interface Instrumentor extends Component {
 
-    /**
-     * Checks if is supported.
-     *
-     * @param processBundle the process bundle
-     * @return true, if is supported
-     */
-    boolean isSupported(ProcessBundle processBundle);
-    
     /**
      * Instrument.
      *
      * @param processBundle the process bundle
      * @throws InstrumentationException the instrumentation exception
      */
-    void instrument(T processBundle) throws InstrumentationException;
+    void instrument(Bundle processBundle) throws InstrumentationException;
     
 }
