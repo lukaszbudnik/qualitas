f048cdf6ec1f7a8e571159e4eb3c1744c70f6d8b
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-02-20 07:25:46 +0100

    introduced abstract component, added its test

---
+++ qualitas-engines-api-core/src/main/java/com/google/code/qualitas/engines/api/component/AbstractComponent.java
@@ -0,0 +1,34 @@
+package com.google.code.qualitas.engines.api.component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.bind.JAXB;
+
+import com.google.code.p.qualitas.engines.api.configuration.QualitasConfiguration;
+import com.google.code.qualitas.engines.api.core.Bundle;
+
+/**
+ * The Class AbstractComponent.
+ */
+public abstract class AbstractComponent implements Component {
+
+    /**
+     * Gets the qualitas configuration.
+     * 
+     * @param bundle
+     *            the bundle
+     * @return the qualitas configuration
+     * @throws IOException
+     *             the IO exception
+     */
+    public QualitasConfiguration getQualitasConfiguration(Bundle bundle) throws IOException {
+        InputStream xml = new ByteArrayInputStream(bundle.getQualitasConfiguration().getContent());
+        QualitasConfiguration qualitasConfiguration = JAXB.unmarshal(xml,
+                QualitasConfiguration.class);
+
+        return qualitasConfiguration;
+    }
+
+}
---
+++ qualitas-engines-api-core/src/test/java/com/google/code/qualitas/engines/api/component/AbstractComponentTest.java
@@ -0,0 +1,40 @@
+package com.google.code.qualitas.engines.api.component;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.google.code.p.qualitas.engines.api.configuration.QualitasConfiguration;
+import com.google.code.qualitas.engines.api.core.Bundle;
+import com.google.code.qualitas.engines.api.core.Entry;
+
+public class AbstractComponentTest {
+
+    private AbstractComponent abstractComponent;
+    private Bundle bundle;
+
+    @Before
+    public void setUp() throws Exception {
+        bundle = Mockito.mock(Bundle.class);
+        byte[] qualitasConfiguration = IOUtils.toByteArray(this.getClass().getResourceAsStream(
+                "/qualitas.xml"));
+        Mockito.when(bundle.getQualitasConfiguration()).thenReturn(
+                new Entry(Bundle.QUALITAS_CONFIGURATION_NAME, qualitasConfiguration));
+
+        abstractComponent = Mockito.mock(AbstractComponent.class);
+        Mockito.when(abstractComponent.getQualitasConfiguration(bundle)).thenCallRealMethod();
+    }
+
+    @Test
+    public void testGetQualitasConfiguration() throws IOException {
+        QualitasConfiguration qualitasConfiguration = abstractComponent
+                .getQualitasConfiguration(bundle);
+
+        Assert.assertNotNull(qualitasConfiguration);
+    }
+
+}
