30e6917a779c8b9b2d86a8ec235b5b257ed74c12
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-02-21 16:49:58 +0100

    added code which allows passing script's arguments directly to maven

--- install_and_validate_all_projects.sh
+++ install_and_validate_all_projects.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 projects=('qualitas' 'qualitas-buildtools' 'qualitas-parent' 'qualitas-utils-zip' 'qualitas-utils-xslt' 'qualitas-engines-api-configuration' 'qualitas-engines-api-core' 'qualitas-engines-api-deployment' 'qualitas-engines-api-factory' 'qualitas-engines-api-instrumentation' 'qualitas-engines-api-invocation' 'qualitas-engines-api-validation' 'qualitas-engines-api-resolution' 'qualitas-engines-ode-core' 'qualitas-engines-ode-deployment' 'qualitas-engines-ode-factory' 'qualitas-engines-ode-instrumentation' 'qualitas-engines-ode-invocation' 'qualitas-engines-ode-validation' 'qualitas-engines-ode-resolution' 'qualitas-engines-ode-spring' 'qualitas-integration-api' 'qualitas-integration-amqp' 'qualitas-services-security-spring' 'qualitas-internal-installation' 'qualitas-webapp');
 for ((i=0; i < ${#projects[@]}; i++))
 do
@@ -8,7 +9,7 @@ do
   echo "------------------------------------------------------------------------------";
   echo "";
   cd ${projects[i]};
-  mvn clean compiler:compile install
+  mvn ${@} clean compiler:compile install
   if [ $? -ne 0 ]
   then
     echo "";
