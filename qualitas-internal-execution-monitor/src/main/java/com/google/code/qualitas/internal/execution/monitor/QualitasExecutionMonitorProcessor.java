5cd982b3e1b414d368b980ddf063314ff531a7d3
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-03-06 12:59:23 +0100

    initial implementation of qualitas execution monitor web service as a camel route

---
+++ qualitas-internal-execution-monitor/src/main/java/com/google/code/qualitas/internal/execution/monitor/QualitasExecutionMonitorProcessor.java
@@ -0,0 +1,30 @@
+package com.google.code.qualitas.internal.execution.monitor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.cxf.message.MessageContentsList;
+import org.springframework.stereotype.Component;
+
+/**
+ * The Class QualitasExecutionMonitorProcessor.
+ */
+@Component
+public class QualitasExecutionMonitorProcessor implements Processor {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.apache.camel.Processor#process(org.apache.camel.Exchange)
+     */
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        MessageContentsList messageContentsList = exchange.getIn().getBody(
+                MessageContentsList.class);
+
+        for (Object o : messageContentsList) {
+            System.out.print(o.getClass() + ": ");
+            System.out.println(o);
+        }
+    }
+}
