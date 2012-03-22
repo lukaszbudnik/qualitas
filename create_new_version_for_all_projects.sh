b5f905a60d2552eb5a489b028ed471b63e8636aa
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-03-22 10:57:36 +0100

    new version of script - now works much better

--- create_new_version_for_all_projects.sh
+++ create_new_version_for_all_projects.sh
@@ -6,20 +6,17 @@ then
   exit 1;
 fi
 
-projects=(`ls | grep qualitas`)
+# root project
+
+projects=(qualitas)
 
 for ((i=0; i < ${#projects[@]}; i++))
 do
-  if [ ${projects[i]} == "qualitas" -o ${projects[i]} == "qualitas-parent" ]
-  then
-    echo "Skipping ${projects[i]}";
-    continue;
-  fi
   echo "------------------------------------------------------------------------------";
   echo "Changing version of ${projects[i]} to $newVersion";
   echo "------------------------------------------------------------------------------";
   cd ${projects[i]};
-  mvn -q versions:set -DnewVersion=$newVersion -DallowSnapshots=true
+  mvn -q -P fastinstall versions:set -DnewVersion=$newVersion
   if [ $? -ne 0 ]
   then
     echo "------------------------------------------------------------------------------";
@@ -28,34 +25,13 @@ do
     echo "";
     exit;
   fi
+  mvn -q -P fastinstall install
   cd ..
 done
 
-cd qualitas-buildtools
-mvn -q install
-cd ..
+# parent project
 
-projects=(qualitas-parent qualitas)
-
-for ((i=0; i < ${#projects[@]}; i++))
-do
-  echo "------------------------------------------------------------------------------";
-  echo "Changing version of ${projects[i]} to $newVersion";
-  echo "------------------------------------------------------------------------------";
-  cd ${projects[i]};
-  mvn -q versions:set install -DnewVersion=$newVersion
-  if [ $? -ne 0 ]
-  then
-    echo "------------------------------------------------------------------------------";
-    echo "Setting new version of ${projects[i]} failed, please see error message above.";
-    echo "------------------------------------------------------------------------------";
-    echo "";
-    exit;
-  fi
-  cd ..
-done
-
-projects=(`ls | grep qualitas`)
+projects=('qualitas' 'qualitas-buildtools' 'qualitas-parent' 'qualitas-utils-zip' 'qualitas-utils-xslt' 'qualitas-utils-dom' 'qualitas-engines-api-configuration' 'qualitas-engines-api-core' 'qualitas-engines-api-deployment' 'qualitas-engines-api-factory' 'qualitas-engines-api-instrumentation' 'qualitas-engines-api-invocation' 'qualitas-engines-api-validation' 'qualitas-engines-api-resolution' 'qualitas-engines-ode-core' 'qualitas-engines-ode-deployment' 'qualitas-engines-ode-factory' 'qualitas-engines-ode-instrumentation' 'qualitas-engines-ode-invocation' 'qualitas-engines-ode-validation' 'qualitas-engines-ode-resolution' 'qualitas-engines-ode-spring' 'qualitas-integration-api' 'qualitas-integration-amqp' 'qualitas-services-security-spring' 'qualitas-execution-monitor-webservice' 'qualitas-internal-bpel-instrumentation' 'qualitas-internal-bpel-spring' 'qualitas-internal-execution-monitor' 'qualitas-internal-installation' 'qualitas-webapp');
 
 for ((i=0; i < ${#projects[@]}; i++))
 do
@@ -63,7 +39,7 @@ do
   echo "Changing parent version of ${projects[i]} to $newVersion";
   echo "------------------------------------------------------------------------------";
   cd ${projects[i]};
-  mvn -q versions:update-parent -DallowSnapshots=true
+  mvn -q -P fastinstall versions:update-parent clean -DparentVersion=$newVersion -DallowSnapshots=true
   if [ $? -ne 0 ]
   then
     echo "------------------------------------------------------------------------------";
@@ -72,6 +48,7 @@ do
     echo "";
     exit;
   fi
+  mvn -q -P fastinstall install  
   cd ..
 done
 
