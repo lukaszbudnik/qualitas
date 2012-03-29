2a8c20e8a9ba8c792180d153e9b4e3fe7bd3f765
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-03-29 16:48:26 +0200

    added spring binding for model project

---
+++ qualitas-internal-model-spring/src/test/java/com/googlecode/qualitas/internal/model/spring/IoCTest.java
@@ -0,0 +1,28 @@
+package com.googlecode.qualitas.internal.model.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.googlecode.qualitas.internal.dao.Repository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/META-INF/spring/qualitas-internal-model-context.xml")
+public class IoCTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void testCreateRepository() {
+        Repository repository = (Repository) context.getBean("repository");
+        
+        Assert.assertNotNull(repository);
+    }
+    
+    
+}
