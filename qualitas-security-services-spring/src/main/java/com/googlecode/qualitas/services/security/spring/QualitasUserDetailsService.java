711976bf6821ccd00cc890402210de9b3049bdf7
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-04-20 16:40:32 +0200

    added logic for creating account when user authenticates for the first time

--- qualitas-security-services-spring/src/main/java/com/googlecode/qualitas/services/security/spring/QualitasUserDetailsService.java
+++ qualitas-security-services-spring/src/main/java/com/googlecode/qualitas/services/security/spring/QualitasUserDetailsService.java
@@ -3,12 +3,16 @@ package com.googlecode.qualitas.services.security.spring;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import com.googlecode.qualitas.internal.dao.Repository;
+import com.googlecode.qualitas.internal.model.User_;
 
 /**
  * The Class QualitasUserDetailsService.
@@ -19,6 +23,10 @@ public class QualitasUserDetailsService implements UserDetailsService {
     /** The Constant QUALITAS_ROLE_USER. */
     private static final String QUALITAS_ROLE_USER = "ROLE_USER";
 
+    /** The repository. */
+    @Autowired
+    private Repository repository;
+
     /*
      * (non-Javadoc)
      * 
@@ -27,12 +35,23 @@ public class QualitasUserDetailsService implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) {
+
+        com.googlecode.qualitas.internal.model.User user = repository.getSingleResultBySingularAttribute(
+                com.googlecode.qualitas.internal.model.User.class, User_.openIDUsername, username);
+        
+        if (user == null) {
+            user = new com.googlecode.qualitas.internal.model.User();
+            user.setOpenIDUsername(username);
+            repository.persist(user);
+        }
+
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         authorities.add(new GrantedAuthorityImpl(QUALITAS_ROLE_USER));
 
-        // password cannot be null or empty, otherwise there is IllegalArgumentException thrown
-        User user = new User(username, "[PROTECTED]", true, true, true, true, authorities);
-        return user;
+        // password cannot be null or empty, otherwise there is
+        // IllegalArgumentException thrown
+        UserDetails userDetails = new User(username, "[PROTECTED]", true, true, true, true, authorities);
+        return userDetails;
     }
 
 }
