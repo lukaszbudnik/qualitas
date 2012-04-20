771ba526d02ac19a84df0dd112810c5c3c77220a
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-04-20 16:38:43 +0200

    added EJB 3.0 specific transaction attributes

--- qualitas-internal-model/src/main/java/com/googlecode/qualitas/internal/dao/Repository.java
+++ qualitas-internal-model/src/main/java/com/googlecode/qualitas/internal/dao/Repository.java
@@ -2,17 +2,26 @@ package com.googlecode.qualitas.internal.dao;
 
 import java.util.List;
 
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.SingularAttribute;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * The Class Repository.
  */
 public class Repository {
+
+    /** The Constant LOG. */
+    private static final Log LOG = LogFactory.getLog(Repository.class);
 
     /** The entity manageer. */
     @PersistenceContext
@@ -26,6 +35,7 @@ public class Repository {
      * @param entity
      *            the entity
      */
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public <T> void persist(T entity) {
         em.persist(entity);
     }
@@ -39,6 +49,7 @@ public class Repository {
      *            the entity
      * @return the t
      */
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public <T> T merge(T entity) {
         return em.merge(entity);
     }
@@ -79,7 +90,16 @@ public class Repository {
         CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
         Root<T> root = criteriaQuery.from(clazz);
         criteriaQuery.where(criteriaBuilder.equal(root.get(attribute), value));
-        return em.createQuery(criteriaQuery).getSingleResult();
+
+        T entity = null;
+        try {
+            entity = em.createQuery(criteriaQuery).getSingleResult();
+        } catch (NoResultException e) {
+            LOG.debug("Select single returned no data for " + clazz + " attribute " + attribute
+                    + " and value " + value);
+        }
+
+        return entity;
     }
 
     /**
