61abce34617f18b5811e3446f83c54e5a075c643
Lukasz Budnik <lukasz.budnik@gmail.com>
2012-05-15 14:00:03 +0200

    added copying bodies and headers using aspectj

--- qualitas-internal-installation-camel/src/main/java/com/googlecode/qualitas/internal/installation/core/CamelHeadersCopierAspect.java
+++ qualitas-internal-installation-camel/src/main/java/com/googlecode/qualitas/internal/installation/core/CamelHeadersCopierAspect.java
@@ -6,18 +6,30 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+/**
+ * The Class CamelHeadersCopierAspect.
+ */
 @Aspect
 public class CamelHeadersCopierAspect {
     
+    /**
+     * Copy headers and body.
+     *
+     * @param pjp the pjp
+     * @param exchange the exchange
+     * @return the object
+     * @throws Throwable the throwable
+     */
     @Around("execution(* com.googlecode.qualitas.internal.installation..*.process(org.apache.camel.Exchange)) && args(exchange) && target(org.apache.camel.Processor)")
     public Object copyHeadersAndBody(ProceedingJoinPoint pjp, Exchange exchange) throws Throwable {
         Object retValue = pjp.proceed();
         
         Message in = exchange.getIn();
         Message out = exchange.getOut();
-        
+        // always copy headers
         out.setHeaders(in.getHeaders());
         
+        // if output body is empty copy it from input
         if (out.getBody() == null) {
             out.setBody(in.getBody());
         }
